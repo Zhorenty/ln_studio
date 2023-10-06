7aa55ea9d9d0a4ad99543edc1a9d0784b145069c
George <zhorenty@gmail.com>
2023-10-06 15:18:12 +0300

    Боже мой что я сделал

--- lib/src/feature/record/widget/components/custom_table_calendar.dart
+++ lib/src/feature/record/widget/components/custom_table_calendar.dart
@@ -41,115 +41,107 @@ class CustomTableCalendar extends StatelessWidget {
       0,
     );
 
-    return Container(
-      padding: const EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        color: context.colorScheme.background,
-        borderRadius: BorderRadius.circular(16),
-        border: Border.all(color: const Color(0xFF272727)),
+    return TableCalendar(
+      availableGestures: AvailableGestures.horizontalSwipe,
+      daysOfWeekStyle: DaysOfWeekStyle(
+        weekdayStyle: context.textTheme.labelLarge!.copyWith(
+          fontFamily: FontFamily.geologica,
+          color: context.colorScheme.secondaryContainer,
+          fontSize: 13,
+        ),
+        weekendStyle: context.textTheme.labelLarge!.copyWith(
+          fontFamily: FontFamily.geologica,
+          color: context.colorScheme.secondaryContainer,
+          fontSize: 13,
+        ),
       ),
-      child: TableCalendar(
-        availableGestures: AvailableGestures.horizontalSwipe,
-        daysOfWeekStyle: DaysOfWeekStyle(
-          weekdayStyle: context.textTheme.labelLarge!.copyWith(
-            fontFamily: FontFamily.geologica,
-            color: context.colorScheme.secondaryContainer,
-            fontSize: 13,
-          ),
-          weekendStyle: context.textTheme.labelLarge!.copyWith(
-            fontFamily: FontFamily.geologica,
-            color: context.colorScheme.secondaryContainer,
-            fontSize: 13,
-          ),
+      locale: 'ru_RU',
+      firstDay: firstDayOfPreviousMonth,
+      lastDay: lastDayOfNextMonth,
+      focusedDay: focusedDay,
+      startingDayOfWeek: StartingDayOfWeek.monday,
+      onDaySelected: onDaySelected,
+      onPageChanged: onPageChanged,
+      headerStyle: HeaderStyle(
+        formatButtonVisible: false,
+        titleCentered: true,
+        titleTextStyle: context.textTheme.bodyLarge!.copyWith(
+          fontFamily: FontFamily.geologica,
         ),
-        locale: 'ru_RU',
-        firstDay: firstDayOfPreviousMonth,
-        lastDay: lastDayOfNextMonth,
-        focusedDay: focusedDay,
-        startingDayOfWeek: StartingDayOfWeek.monday,
-        onDaySelected: onDaySelected,
-        onPageChanged: onPageChanged,
-        headerStyle: HeaderStyle(
-          formatButtonVisible: false,
-          titleCentered: true,
-          titleTextStyle: context.textTheme.bodyLarge!.copyWith(
-            fontFamily: FontFamily.geologica,
-          ),
-          leftChevronVisible: false,
-          rightChevronVisible: false,
+        leftChevronVisible: false,
+        rightChevronVisible: false,
+      ),
+      selectedDayPredicate: selectedDayPredicate,
+      enabledDayPredicate: enabledDayPredicate,
+      calendarStyle: CalendarStyle(
+        // Text styles
+        selectedTextStyle: context.textTheme.titleSmall!.copyWith(
+          fontFamily: FontFamily.geologica,
+          fontWeight: FontWeight.bold,
+          color: context.colorScheme.background,
         ),
-        selectedDayPredicate: selectedDayPredicate,
-        enabledDayPredicate: enabledDayPredicate,
-        calendarStyle: CalendarStyle(
-          // Text styles
-          selectedTextStyle: context.textTheme.titleSmall!.copyWith(
-            fontFamily: FontFamily.geologica,
-            fontWeight: FontWeight.bold,
-            color: context.colorScheme.background,
-          ),
-          todayTextStyle: context.textTheme.titleSmall!.copyWith(
-            fontFamily: FontFamily.geologica,
-            color: context.colorScheme.secondary,
-          ),
-          defaultTextStyle: context.textTheme.titleSmall!.copyWith(
-            fontFamily: FontFamily.geologica,
-            fontWeight: FontWeight.bold,
-            color: context.colorScheme.secondary,
-          ),
-          holidayTextStyle: context.textTheme.titleSmall!.copyWith(
-            fontFamily: FontFamily.geologica,
-            color: context.colorScheme.secondary,
-          ),
-          weekendTextStyle: context.textTheme.titleSmall!.copyWith(
-            fontFamily: FontFamily.geologica,
-            color: context.colorScheme.secondary,
-          ),
-          outsideTextStyle: context.textTheme.titleSmall!.copyWith(
-            fontFamily: FontFamily.geologica,
-            fontWeight: FontWeight.bold,
-            color: context.colorScheme.secondary,
-          ),
-          disabledTextStyle: context.textTheme.titleSmall!.copyWith(
-            fontFamily: FontFamily.geologica,
-            color: const Color(0xFF555555),
-          ),
+        todayTextStyle: context.textTheme.titleSmall!.copyWith(
+          fontFamily: FontFamily.geologica,
+          color: context.colorScheme.secondary,
+        ),
+        defaultTextStyle: context.textTheme.titleSmall!.copyWith(
+          fontFamily: FontFamily.geologica,
+          fontWeight: FontWeight.bold,
+          color: context.colorScheme.secondary,
+        ),
+        holidayTextStyle: context.textTheme.titleSmall!.copyWith(
+          fontFamily: FontFamily.geologica,
+          color: context.colorScheme.secondary,
+        ),
+        weekendTextStyle: context.textTheme.titleSmall!.copyWith(
+          fontFamily: FontFamily.geologica,
+          color: context.colorScheme.secondary,
+        ),
+        outsideTextStyle: context.textTheme.titleSmall!.copyWith(
+          fontFamily: FontFamily.geologica,
+          fontWeight: FontWeight.bold,
+          color: context.colorScheme.secondary,
+        ),
+        disabledTextStyle: context.textTheme.titleSmall!.copyWith(
+          fontFamily: FontFamily.geologica,
+          color: const Color(0xFF555555),
+        ),
 
-          // Decorations
-          selectedDecoration: BoxDecoration(
-            shape: BoxShape.rectangle,
-            borderRadius: const BorderRadius.all(Radius.circular(8)),
-            color: context.colorScheme.primary,
-          ),
-          disabledDecoration: BoxDecoration(
-            shape: BoxShape.rectangle,
-            borderRadius: const BorderRadius.all(Radius.circular(8)),
-            color: context.colorScheme.scrim,
-          ),
-          defaultDecoration: BoxDecoration(
-            shape: BoxShape.rectangle,
-            borderRadius: const BorderRadius.all(Radius.circular(8)),
-            color: context.colorScheme.scrim,
-          ),
-          weekendDecoration: BoxDecoration(
-            shape: BoxShape.rectangle,
-            borderRadius: const BorderRadius.all(Radius.circular(8)),
-            color: context.colorScheme.scrim,
-          ),
-          holidayDecoration: BoxDecoration(
-            shape: BoxShape.rectangle,
-            borderRadius: const BorderRadius.all(Radius.circular(8)),
-            color: context.colorScheme.scrim,
-          ),
-          outsideDecoration: BoxDecoration(
-            shape: BoxShape.rectangle,
-            borderRadius: const BorderRadius.all(Radius.circular(8)),
-            color: context.colorScheme.scrim,
-          ),
-          todayDecoration: BoxDecoration(
-            shape: BoxShape.rectangle,
-            borderRadius: const BorderRadius.all(Radius.circular(8)),
-            color: context.colorScheme.scrim,
-          ),
+        // Decorations
+        selectedDecoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          borderRadius: const BorderRadius.all(Radius.circular(8)),
+          color: context.colorScheme.primary,
+        ),
+        disabledDecoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          borderRadius: const BorderRadius.all(Radius.circular(8)),
+          color: context.colorScheme.scrim,
+        ),
+        defaultDecoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          borderRadius: const BorderRadius.all(Radius.circular(8)),
+          color: context.colorScheme.scrim,
+        ),
+        weekendDecoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          borderRadius: const BorderRadius.all(Radius.circular(8)),
+          color: context.colorScheme.scrim,
+        ),
+        holidayDecoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          borderRadius: const BorderRadius.all(Radius.circular(8)),
+          color: context.colorScheme.scrim,
+        ),
+        outsideDecoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          borderRadius: const BorderRadius.all(Radius.circular(8)),
+          color: context.colorScheme.scrim,
+        ),
+        todayDecoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          borderRadius: const BorderRadius.all(Radius.circular(8)),
+          color: context.colorScheme.scrim,
         ),
       ),
     );
