34b48c731e76ee1e29de6d15c2e5d0b2b0474639
erik.minasov <erik.minasov@rt.ru>
2023-12-22 10:49:38 +0300

    Рефактор виджета карт

--- lib/src/common/widget/maps_sheet.dart
+++ lib/src/common/widget/maps_sheet.dart
@@ -6,34 +6,31 @@ import 'package:map_launcher/map_launcher.dart';
 Future<void> showMapsSheet({
   required BuildContext context,
   required Coords coords,
-  String? title,
-  String? description,
+  required String title,
 }) =>
     MapLauncher.installedMaps.then(
       (availableMaps) => ModalPopup.show(
         context: context,
-        child: SingleChildScrollView(
-          child: Wrap(
-            children: [
-              for (var map in availableMaps)
-                ListTile(
-                  onTap: () => map.showMarker(
-                    coords: Coords(45.019641, 39.025248),
-                    title: title ?? 'Oko lashes',
-                    description: 'Уход за ресницами',
-                  ),
-                  title: Text(map.mapName),
-                  leading: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: SvgPicture.asset(
-                      map.icon,
-                      height: 50,
-                      width: 50,
-                    ),
-                  ),
+        child: ListView.builder(
+          itemCount: availableMaps.length,
+          itemBuilder: (context, index) {
+            final map = availableMaps[index];
+            return ListTile(
+              onTap: () => map.showMarker(
+                coords: coords,
+                title: title,
+              ),
+              title: Text(map.mapName),
+              leading: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: SvgPicture.asset(
+                  map.icon,
+                  height: 50,
+                  width: 50,
                 ),
-            ],
-          ),
+              ),
+            );
+          },
         ),
       ),
     );
